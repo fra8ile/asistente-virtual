35a8dc084f66850ccb1886fbe29df75315de015c
agostinaCer01 <agos_1234@outlook.com>
2026-06-13 19:29:52 -0300

    ds

--- asistente.py
+++ asistente.py
@@ -1,4 +1,4 @@
-from recordatorios import recordatorio, agregar_recordatorio, ver_recordatorios, modificar_recordatorio
+from recordatorios import menu_recordatorio
 from estudiante import agregar_materia,modulo_estudiante,ver_materias
 from gastos import agregar_gasto,modulo_gastos,total_por_categoria,total_mes,gasto_mas_alto
 from interfaz import pedir_input,limpiar_pantalla,menu_inicio
@@ -19,7 +19,7 @@ def asistente():
         if opcion == 1:
             modulo_estudiante()
         elif opcion == 2:
-            recordatorio()
+            menu_recordatorio()
         elif opcion == 3:
             modulo_gastos()
         elif opcion == 4:
